c90d659ccfcbc0929a5ccbf2671ebc8a9bae6d6d
pazukdev <pazukdev@gmail.com>
2019-04-21 19:13:48 +0300

    Initial commit

---
+++ src/main/java/com/pazukdev/bearingsinfo/BearingsInfoApplication.java
@@ -0,0 +1,13 @@
+package com.pazukdev.bearingsinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearingsInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BearingsInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pazukdev/bearingsinfo/BearingsInfoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pazukdev.bearingsinfo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BearingsInfoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
