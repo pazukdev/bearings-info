338379c47ad64b19881a17ffb5ebcb30040f33ba
pazukdev <pazukdev@gmail.com>
2019-05-05 17:38:26 +0300

    tests: unit tests: BearingServiceTest added

---
+++ src/test/java/com/pazukdev/bearingsinfo/service/BearingServiceTest.java
@@ -0,0 +1,69 @@
+package com.pazukdev.bearingsinfo.service;
+
+import com.pazukdev.bearingsinfo.MockData;
+import com.pazukdev.bearingsinfo.converter.BearingConverter;
+import com.pazukdev.bearingsinfo.dbo.Bearing;
+import com.pazukdev.bearingsinfo.dto.BearingDto;
+import com.pazukdev.bearingsinfo.repository.BearingRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+/**
+ * @author Siarhei Sviarkaltsau
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class BearingServiceTest {
+
+    @InjectMocks
+    private BearingService service;
+    @Mock
+    private BearingRepository repository;
+    @Spy
+    private BearingConverter converter;
+
+    @Test
+    public void createBearing() {
+        final Bearing bearing = MockData.bearing();
+
+        doReturn(bearing).when(repository).save(any(Bearing.class));
+        service.createBearing(new BearingDto());
+
+        verify(repository, times(1)).save(any(Bearing.class));
+
+    }
+
+    @Test
+    public void getBearingsList() {
+        final Bearing bearing = MockData.bearing();
+
+        final List<Bearing> findAllResult = new ArrayList<>();
+        findAllResult.add(bearing);
+        findAllResult.add(bearing);
+
+        doReturn(findAllResult).when(repository).findAll();
+        final List<BearingDto> bearingsList = service.getBearingsList();
+
+        verify(repository, times(1)).findAll();
+        assertEquals(findAllResult.size(), bearingsList.size());
+        for (final BearingDto bearingDto : bearingsList) {
+            assertEquals(bearing.getName(), bearingDto.getName());
+            assertEquals(bearing.getMajorLocation(), bearingDto.getMajorLocation());
+            assertEquals(bearing.getQuantity(), bearingDto.getQuantity());
+        }
+
+    }
+
+}
