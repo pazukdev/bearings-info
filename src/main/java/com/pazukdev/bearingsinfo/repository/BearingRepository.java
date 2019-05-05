ea60743b6e7a2d6d719e2cd77925adf42d2ebf21
pazukdev <pazukdev@gmail.com>
2019-05-05 16:14:40 +0300

    fix for BearingRepository: unnecessary List<Bearing> findAll() method removed

--- src/main/java/com/pazukdev/bearingsinfo/repository/BearingRepository.java
+++ src/main/java/com/pazukdev/bearingsinfo/repository/BearingRepository.java
@@ -3,13 +3,7 @@ package com.pazukdev.bearingsinfo.repository;
 import com.pazukdev.bearingsinfo.dbo.Bearing;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 /**
  * @author Siarhei Sviarkaltsau
  */
-public interface BearingRepository extends JpaRepository<Bearing, Long> {
-
-    List<Bearing> findAll();
-
-}
+public interface BearingRepository extends JpaRepository<Bearing, Long> {}
