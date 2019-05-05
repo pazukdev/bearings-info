c1fd0e7b5508409cca5304935ffa8368ae10de03
pazukdev <pazukdev@gmail.com>
2019-05-05 18:20:51 +0300

    interface DboDtoConverter<Dbo, Dto>: minor update

--- src/main/java/com/pazukdev/bearingsinfo/converter/DboDtoConverter.java
+++ src/main/java/com/pazukdev/bearingsinfo/converter/DboDtoConverter.java
@@ -1,11 +1,13 @@
 package com.pazukdev.bearingsinfo.converter;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * @author Siarhei Sviarkaltsau
  */
 public interface DboDtoConverter<Dbo, Dto> {
 
-    Dto convertToDto(final Dbo dbo);
-    Dbo convertToDbo(final Dto dto);
+    Dto convertToDto(@NotNull final Dbo dbo);
+    Dbo convertToDbo(@NotNull final Dto dto);
 
 }
