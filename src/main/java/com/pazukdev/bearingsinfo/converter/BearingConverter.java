b292ab98b242faa293e25ddbdc594337dbe1027d
pazukdev <pazukdev@gmail.com>
2019-05-05 18:35:42 +0300

    BearingConverter: update: method implementation changed to based on org.modelmapper.ModelMapper

--- src/main/java/com/pazukdev/bearingsinfo/converter/BearingConverter.java
+++ src/main/java/com/pazukdev/bearingsinfo/converter/BearingConverter.java
@@ -2,29 +2,22 @@ package com.pazukdev.bearingsinfo.converter;
 
 import com.pazukdev.bearingsinfo.dbo.Bearing;
 import com.pazukdev.bearingsinfo.dto.BearingDto;
+import org.modelmapper.ModelMapper;
 
 /**
  * @author Siarhei Sviarkaltsau
  */
 public class BearingConverter implements DboDtoConverter<Bearing, BearingDto> {
 
+    private static final ModelMapper modelMapper = new ModelMapper();
+
     @Override
     public BearingDto convertToDto(final Bearing bearing) {
-        final BearingDto bearingDto = new BearingDto();
-        bearingDto.setId(bearing.getId());
-        bearingDto.setName(bearing.getName());
-        bearingDto.setMajorLocation(bearing.getMajorLocation());
-        bearingDto.setQuantity(bearing.getQuantity());
-        return bearingDto;
+        return modelMapper.map(bearing, BearingDto.class);
     }
 
     @Override
     public Bearing convertToDbo(final BearingDto bearingDto) {
-        final Bearing bearing = new Bearing();
-        bearing.setId(bearingDto.getId());
-        bearing.setName(bearingDto.getName());
-        bearing.setMajorLocation(bearingDto.getMajorLocation());
-        bearing.setQuantity(bearingDto.getQuantity());
-        return bearing;
+        return modelMapper.map(bearingDto, Bearing.class);
     }
 }
