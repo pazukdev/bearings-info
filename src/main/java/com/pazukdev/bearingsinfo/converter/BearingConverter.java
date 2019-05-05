be83312982c430d177d132fd4d9b8eaf11a6d9b5
pazukdev <pazukdev@gmail.com>
2019-05-05 18:20:24 +0300

    BearingConverter: DboDtoConverter<Dbo, Dto> interface methods are implemented

--- src/main/java/com/pazukdev/bearingsinfo/converter/BearingConverter.java
+++ src/main/java/com/pazukdev/bearingsinfo/converter/BearingConverter.java
@@ -10,11 +10,21 @@ public class BearingConverter implements DboDtoConverter<Bearing, BearingDto> {
 
     @Override
     public BearingDto convertToDto(final Bearing bearing) {
-        return null;
+        final BearingDto bearingDto = new BearingDto();
+        bearingDto.setId(bearing.getId());
+        bearingDto.setName(bearing.getName());
+        bearingDto.setMajorLocation(bearing.getMajorLocation());
+        bearingDto.setQuantity(bearing.getQuantity());
+        return bearingDto;
     }
 
     @Override
-    public Bearing convertToDbo(final BearingDto bearingDTO) {
-        return null;
+    public Bearing convertToDbo(final BearingDto bearingDto) {
+        final Bearing bearing = new Bearing();
+        bearing.setId(bearingDto.getId());
+        bearing.setName(bearingDto.getName());
+        bearing.setMajorLocation(bearingDto.getMajorLocation());
+        bearing.setQuantity(bearingDto.getQuantity());
+        return bearing;
     }
 }
