c13095cd558eced54b54e7170a09e86c5bb37738
pazukdev <pazukdev@gmail.com>
2019-05-05 18:21:34 +0300

    BearingDto: update: long serialVersionUID added

--- src/main/java/com/pazukdev/bearingsinfo/dto/BearingDto.java
+++ src/main/java/com/pazukdev/bearingsinfo/dto/BearingDto.java
@@ -2,11 +2,15 @@ package com.pazukdev.bearingsinfo.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @author Siarhei Sviarkaltsau
  */
 @Data
-public class BearingDto {
+public class BearingDto implements Serializable {
+
+    private final static long serialVersionUID = 12343L;
 
     private Long id;
     private String name;
