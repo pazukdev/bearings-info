c258259bdc53da52d5c934d150cc9fe87b2b9d5d
pazukdev <pazukdev@gmail.com>
2019-05-29 15:22:25 +0200

    update for swagger config (swagger config was added in previous commit)

--- backend/src/main/java/com/pazukdev/backend/config/SwaggerConfig.java
+++ backend/src/main/java/com/pazukdev/backend/config/SwaggerConfig.java
@@ -3,7 +3,6 @@ package com.pazukdev.backend.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,8 +11,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import javax.sql.DataSource;
-
 /**
  * @author Siarhei Sviarkaltsau
  */
@@ -21,9 +18,6 @@ import javax.sql.DataSource;
 @EnableSwagger2
 @RequiredArgsConstructor
 public class SwaggerConfig {
-
-    private final Jackson2ObjectMapperBuilder objectMapperBuilder;
-    private final DataSource dataSource;
 
     @Bean
     public Docket api() {
@@ -37,8 +31,8 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("bearings-info APIs")
-                .description("APIs for work with bearings-info server")
+                .title("bearings-info API")
+                .description("API for bearings-info server")
                 .build();
     }
 
