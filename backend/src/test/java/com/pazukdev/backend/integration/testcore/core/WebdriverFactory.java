1fc1a56fa08ed34c00bb9a583ed9eedb786dc087
Siarhei Sviarkaltsau <pazuk1985@gmail.com>
2019-06-11 14:42:20 +0300

    headless option added to ChromeDriver

--- backend/src/test/java/com/pazukdev/backend/integration/testcore/core/WebdriverFactory.java
+++ backend/src/test/java/com/pazukdev/backend/integration/testcore/core/WebdriverFactory.java
@@ -2,6 +2,7 @@ package com.pazukdev.backend.integration.testcore.core;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -14,7 +15,9 @@ public class WebdriverFactory {
     @Bean
     public WebDriver createRemoteDriver() {
         System.setProperty("webdriver.chrome.driver", "/opt/chromedriver/chromedriver");
-        return new ChromeDriver();
+        final ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        return new ChromeDriver(chromeOptions);
     }
 
 }
