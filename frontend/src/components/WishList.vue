c027eadc4ef2909b0ff3c839a58ae43ed6e880ef
Siarhei Sviarkaltsau <pazuk1985@gmail.com>
2019-08-22 12:33:05 +0300

    WishList.vue: updated

--- frontend/src/components/WishList.vue
+++ frontend/src/components/WishList.vue
@@ -1,22 +1,54 @@
 <template>
     <div>
-        <p id="title" style="text-align: center"><b>Wishlist</b></p>
-        <table>
+        <table style="text-align: center">
+            <tr>
+                <td style="width: 120px"></td>
+                <td></td>
+                <td style="text-align: right; width: 80px">
+                    <button type="button"
+                            v-on:click="openAddToWishListForm()"
+                            :disabled="selected.length === 0">
+                        Remove
+                    </button>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <p id="title" style="text-align: center;"><b>Wishlist</b></p>
+                </td>
+            </tr>
+            <tr>
+                <td>Category</td>
+                <td>Name</td>
+                <td>Quantity</td>
+            </tr>
+        </table>
+        <table class="get-all-table">
             <tbody>
             <tr v-for="table in wishList.tables">
                 <td>
-                    <table style="text-align: center">
+                    <table class="get-all-table">
                         <tbody>
-                        <tr v-for="row in table.matrix">
+                        <tr>
                             <td style="width: 120px">
+                                <b>{{table.matrix[0][0]}}</b>
+                            </td>
+                            <td></td>
+                            <td style="width: 80px"></td>
+                        </tr>
+                        <tr v-for="row in table.matrix">
+                            <td>
                                 {{row[0]}}
                             </td>
                             <td>
                                 {{row[1]}}
                             </td>
-                            <td style="width: 60px">
+                            <td>
                                 {{row[2]}}
                             </td>
+                        </tr>
+                        <tr>
+                            <p></p>
                         </tr>
                         </tbody>
                     </table>
@@ -34,7 +66,8 @@
 
         data() {
             return {
-
+                selected: [],
+                selectAll: false
             }
         },
 
