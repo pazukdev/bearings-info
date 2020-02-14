630148de744b4b715455938d2c545fa901c96d32
pazukdev <pazukdev@gmail.com>
2020-02-14 16:20:50 +0300

    config for deploy on heroku

--- backend/src/test/java/com/pazukdev/backend/unit/util/TranslationUtilTest.java
+++ backend/src/test/java/com/pazukdev/backend/unit/util/TranslationUtilTest.java
@@ -2,37 +2,15 @@ package com.pazukdev.backend.unit.util;
 
 import com.pazukdev.backend.util.ItemUtil;
 import com.pazukdev.backend.util.TranslatorUtil;
-import org.apache.tika.langdetect.OptimaizeLangDetector;
-import org.apache.tika.language.detect.LanguageDetector;
-import org.apache.tika.language.detect.LanguageResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.junit.Test;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class TranslationUtilTest {
-
-    @Test
-    public void test() throws IOException {
-        String s = null;
-//        s = TranslatorUtil.translate("ru", "en", "Стрекоза",true);
-        System.out.println(s);
-//        s = TranslatorUtil.translate("en", "en", "Rat", true);
-//        System.out.println(s);
-//        s = TranslatorUtil.translate("en", "ru", "Rat", true);
-//        System.out.println(s);
-    }
-
-    @Test
-    public void test2() {
-        LanguageDetector detector = new OptimaizeLangDetector().loadModels();
-        LanguageResult result = detector.detect("суслик");
-        System.out.println(result.getLanguage());
-    }
 
     @Test
     public void test3() {
