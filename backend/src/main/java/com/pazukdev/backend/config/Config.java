b3785bad625c0d6dc5f1f063f2ae10e65d8a0ef4
pazukdev <pazukdev@gmail.com>
2020-05-10 11:30:36 +0300

    emailing implemented via sendgrid

--- backend/src/main/java/com/pazukdev/backend/config/Config.java
+++ backend/src/main/java/com/pazukdev/backend/config/Config.java
@@ -1,25 +1,22 @@
 package com.pazukdev.backend.config;
 
+import com.sendgrid.SendGrid;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.*;
 
-import java.util.Properties;
-
-import static com.pazukdev.backend.util.Util.getMailSenderData;
-
 /**
  * @author Siarhei Sviarkaltsau
  */
 @Configuration
 public class Config {
+
+     private static final String SENDGRID_API_KEY = "SENDGRID_API_KEY";
 
     @Bean
     public ModelMapper getModelMapper() {
@@ -73,19 +70,8 @@ public class Config {
     }
 
     @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-        mailSender.setUsername(getMailSenderData()[0]);
-        mailSender.setPassword(getMailSenderData()[1]);
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-
-        return mailSender;
+    public SendGrid getSendGrid() {
+        return new SendGrid(System.getenv(SENDGRID_API_KEY));
     }
 
 }
--- backend/src/main/java/com/pazukdev/backend/service/EmailSenderService.java
+++ backend/src/main/java/com/pazukdev/backend/service/EmailSenderService.java
@@ -1,11 +1,19 @@
 package com.pazukdev.backend.service;
 
 import com.pazukdev.backend.constant.Constant;
+import com.pazukdev.backend.util.LoggerUtil;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
 
 /**
  * @author Siarhei Sviarkaltsau
@@ -14,62 +22,33 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EmailSenderService {
 
-    private final JavaMailSender javaMailSender;
+    private final SendGrid sendGrid;
 
-    @Async
-    public void send(final SimpleMailMessage email) {
-        try {
-            javaMailSender.send(email);
-        } catch (final Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-//    @Async
-//    public void send(final MimeMessagePreparator message) {
-//        javaMailSender.send(message);
-//    }
-
-//    @Async
-//    public void send(final Object message) {
-//        try {
-//            if (message instanceof SimpleMailMessage) {
-//                javaMailSender.send((SimpleMailMessage) message);
-//            } else if (message instanceof MimeMessagePreparator) {
-//                javaMailSender.send((MimeMessagePreparator) message);
-//            }
-//        } catch (final Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-    public void emailToYourself(final String subject, final String text) {
+    public void emailToYourself(@Nonnull final String subject, @Nonnull final String text) {
         emailTo(Constant.EMAIL, subject, text, false);
     }
 
-    public void emailTo(final String toAddress, final String subject, final String text, final boolean html) {
-        final SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setFrom(Constant.EMAIL);
-        mailMessage.setTo(toAddress);
-        mailMessage.setSubject(subject);
-        mailMessage.setText(text);
-
-        send(mailMessage);
+    public void emailTo(@Nonnull final String toAddress,
+                        @Nonnull final String subject,
+                        @Nonnull final String text,
+                        final boolean html) {
+        final Email from = new Email(Constant.EMAIL);
+        final Email to = new Email(toAddress);
+        final Content content = new Content("text/plain", text);
+        final Mail mail = new Mail(from, subject, to, content);
+        final Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            final Response response = sendGrid.api(request);
+            LoggerUtil.info("status code: " + response.getStatusCode());
+            LoggerUtil.info("response body: " + response.getBody());
+            LoggerUtil.info("response headers: " + response.getHeaders());
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
     }
-
-//    public void emailTo(final String toAddress,
-//                        final String subject,
-//                        final String text,
-//                        final boolean html) {
-//        MimeMessagePreparator message = mimeMessage -> {
-//            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
-//            messageHelper.setFrom(Constant.EMAIL);
-//            messageHelper.setTo(toAddress);
-//            messageHelper.setSubject(subject);
-//            messageHelper.setText(text, html);
-//        };
-//        send(message);
-//    }
 
 }
 
