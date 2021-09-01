1b732b8f738fc9028332bb0c7d45338524e1fb36
pazukdev <pazukdev@gmail.com>
2021-09-01 22:10:59 +0300

    deploy

---
+++ js/chunk-2d0d78b1.3bd3ec6f.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d78b1"],{"76ea":function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",[n("CountedItemList",{attrs:{"summary-view":!0,sorted:!0}})],1)},s=[],u=n("3e98"),o={name:"ItemSummary",components:{CountedItemList:u["default"]}},r=o,c=n("2877"),d=Object(c["a"])(r,a,s,!1,null,"a2c6ae94",null);t["default"]=d.exports}}]);
+//# sourceMappingURL=chunk-2d0d78b1.3bd3ec6f.js.map
